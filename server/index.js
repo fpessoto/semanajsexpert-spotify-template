2b48717250915660f5006110c84d5c2e12dbde09
Felipe Pessoto Medeiros Guimarães <felipe.pessoto@hotmail.com>
2022-03-15 23:22:41 -0300

    created services,controller, routes and config implementation

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+import config from "./config.js";
 import server from "./server.js";
+import { logger } from "./utils.js";
 
-server.listen(3000).on("listening", () => console.log("server running"));
+server
+  .listen(config.port)
+  .on("listening", () => logger.info(`server running at ${config.port}`));
